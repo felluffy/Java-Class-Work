cbf3d51d5877a96c9bc133c59e95fc6d99474ca6
felluffy <somerandomghai@gmail.com>
2017-09-07 12:56:09 +0600

    Add definition for twoOccurences which will check if a string is present more than 1 time in another string.

---
+++ Part3.java
@@ -0,0 +1,33 @@
+public class Part3
+{
+    boolean twoOccurences(String stringa, String stringb)
+    {
+        int count = 0;
+        //for(int index = 0; index != -1; index = stringb.indexOf(stringa, index + stringa.length()))
+        //    count++;
+        int index = 0 - stringa.length();
+        do
+        {
+            index = stringb.indexOf(stringa, index + stringa.length());
+            if(index != -1)
+                count++;
+        }while(index != -1);
+       
+        if(count < 2)
+            return false;
+        else
+            return true;
+    }
+    void test()
+    {
+        String stringa = "by";
+        String stringb = "A story by Abby Long";
+        System.out.println(twoOccurences(stringa, stringb));
+        stringa = "a";
+        stringb = "banana";
+        System.out.println(twoOccurences(stringa, stringb));
+        stringa = "atg";
+        stringb = "ctgtatgta";
+        System.out.println(twoOccurences(stringa, stringb));
+    }
+}
