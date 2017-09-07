c2bdc8a8a4822b343751f293b9f13008988aa264
felluffy <somerandomghai@gmail.com>
2017-09-07 11:47:10 +0600

    Mod findSimpleGene to work with specific start and end codons

--- Part2.java
+++ Part2.java
@@ -1,9 +1,9 @@
 public class Part2
 {
-    String findSimpleGene(String DNA)
+    String findSimpleGene(String DNA, String startCodon, String endCodon)
     {
-        int start = DNA.indexOf("ATG");
-        int end = DNA.indexOf("TAA", start + 3);
+        int start = DNA.indexOf(startCodon);
+        int end = DNA.indexOf(endCodon, start + 3);
         if(end == -1 || start == -1 || (end - start)%3 != 0)
             return "";
         else 
@@ -14,27 +14,29 @@ public class Part2
     {
         //NO ATG AND TAA
         String DNA = "ATACGATATTATACCCGAGAT";
-        String gene = findSimpleGene(DNA);
+        String gene = findSimpleGene(DNA, "ATG", "TAA");
         System.out.println("Gene: " + gene);
         
         //NO ATG
         DNA = "ATACGATATTATAACCCGAGAT";
-        gene = findSimpleGene(DNA);
+        gene = findSimpleGene(DNA, "ATG", "TAA");
         System.out.println("Gene: " + gene);
         
         //NO TAA
         DNA = "ATGACGATATTATACCCGAGAT";
-        gene = findSimpleGene(DNA);
+        gene = findSimpleGene(DNA, "ATG", "TAA");
         System.out.println("Gene: " + gene);
         
         //Multiple of 3
         DNA = "ATGACGATATAATATAACCCGAGAT";
-        gene = findSimpleGene(DNA);
+        gene = findSimpleGene(DNA, "ATG", "TAA");
         System.out.println("Gene: " + gene);
         
         //Not a multiple of 3
         DNA = "ATGACGATAGTAATATAACCCGAGAT";
-        gene = findSimpleGene(DNA);
+        gene = findSimpleGene(DNA, "ATG", "TAA");
         System.out.println("Gene: " + gene);
+        
+        //System.out.println("Finished testSimpleGene");
     }
 }
