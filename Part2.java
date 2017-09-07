c6b0cf00331090bb143d34f911ce85da3f5ab0c0
felluffy <somerandomghai@gmail.com>
2017-09-07 11:42:03 +0600

    Copy contents of Part1 to Part2

---
+++ Part2.java
@@ -0,0 +1,40 @@
+public class Part2
+{
+    String findSimpleGene(String DNA)
+    {
+        int start = DNA.indexOf("ATG");
+        int end = DNA.indexOf("TAA", start + 3);
+        if(end == -1 || start == -1 || (end - start)%3 != 0)
+            return "";
+        else 
+            return DNA.substring(start, end + 3);
+    }
+    
+    void testSimpleGene()
+    {
+        //NO ATG AND TAA
+        String DNA = "ATACGATATTATACCCGAGAT";
+        String gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        //NO ATG
+        DNA = "ATACGATATTATAACCCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        //NO TAA
+        DNA = "ATGACGATATTATACCCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        //Multiple of 3
+        DNA = "ATGACGATATAATATAACCCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+        
+        //Not a multiple of 3
+        DNA = "ATGACGATAGTAATATAACCCGAGAT";
+        gene = findSimpleGene(DNA);
+        System.out.println("Gene: " + gene);
+    }
+}
