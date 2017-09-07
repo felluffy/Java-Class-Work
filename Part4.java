f79d2f801d8f33d551c2dff243554483ed6c4b4e
felluffy <somerandomghai@gmail.com>
2017-09-07 22:22:21 +0600

    add check method which reads from a url word by word

---
+++ Part4.java
@@ -0,0 +1,12 @@
+import edu.duke.*;
+public class Part4
+{
+    void check()
+    {
+        URLResource url = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        for(String s: url.words())
+        {
+            
+        }
+    }
+}
