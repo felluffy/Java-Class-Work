d42f42766f9a07a3afde525426dad0f9d80aecc5
felluffy <somerandomghai@gmail.com>
2017-09-07 22:52:40 +0600

    mod the check method to print out all the youtube links present in the given url

--- Part4.java
+++ Part4.java
@@ -6,7 +6,16 @@ public class Part4
         URLResource url = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
         for(String s: url.words())
         {
-            
+            //if(s == "youtube.com")
+            //System.out.println(s + " ");    
+            s = s.toLowerCase();
+            int index = s.indexOf("youtube.com");
+            if(index != -1)
+            {
+                index = s.indexOf("\"");
+                int endIndex = s.indexOf("\"", index + 1);
+                System.out.println(s.substring(index + 1, endIndex));
+            }
         }
     }
 }
