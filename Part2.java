83d5f840be61eaa5f9039722220e9c4496883981
felluffy <somerandomghai@gmail.com>
2017-09-07 12:16:51 +0600

    mod findSimpleGene to work with either only uppercase or lowercase DNA starnds by adding two methods(isUpperCase and isLowerCase).

--- Part2.java
+++ Part2.java
@@ -1,9 +1,34 @@
 public class Part2
 {
+    public static boolean isUpperCase(String s)
+    {
+        for (int i=0; i<s.length(); i++)
+        {
+            if (!Character.isUpperCase(s.charAt(i)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isLowerCase(String s)
+    {
+        for (int i=0; i<s.length(); i++)
+        {
+            if (!Character.isLowerCase(s.charAt(i)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     String findSimpleGene(String DNA, String startCodon, String endCodon)
     {
-        int start = DNA.indexOf(startCodon);
-        int end = DNA.indexOf(endCodon, start + 3);
+        if(!isUpperCase(DNA) && !isLowerCase(DNA))
+            return "";
+        String DNACopy = DNA.toUpperCase();
+        int start = DNACopy.indexOf(startCodon.toUpperCase());
+        int end = DNACopy.indexOf(endCodon.toUpperCase(), start + 3);
         if(end == -1 || start == -1 || (end - start)%3 != 0)
             return "";
         else 
@@ -15,28 +40,38 @@ public class Part2
         //NO ATG AND TAA
         String DNA = "ATACGATATTATACCCGAGAT";
         String gene = findSimpleGene(DNA, "ATG", "TAA");
-        System.out.println("Gene: " + gene);
+        System.out.println("\nDNA strand: " + DNA + "\nGene: " + gene);
         
         //NO ATG
         DNA = "ATACGATATTATAACCCGAGAT";
         gene = findSimpleGene(DNA, "ATG", "TAA");
-        System.out.println("Gene: " + gene);
+        System.out.println("DNA strand: " + DNA + "\nGene: " + gene);
         
         //NO TAA
         DNA = "ATGACGATATTATACCCGAGAT";
         gene = findSimpleGene(DNA, "ATG", "TAA");
-        System.out.println("Gene: " + gene);
+        System.out.println("DNA strand: " + DNA + "\nGene: " + gene);
         
         //Multiple of 3
         DNA = "ATGACGATATAATATAACCCGAGAT";
         gene = findSimpleGene(DNA, "ATG", "TAA");
-        System.out.println("Gene: " + gene);
+        System.out.println("DNA strand: " + DNA + "\nGene: " + gene);
         
         //Not a multiple of 3
         DNA = "ATGACGATAGTAATATAACCCGAGAT";
         gene = findSimpleGene(DNA, "ATG", "TAA");
-        System.out.println("Gene: " + gene);
+        System.out.println("DNA strand: " + DNA + "\nGene: " + gene);
         
+        //Has both lower and upper case letters
+        DNA = "aTgacgataTaatataacccgata";
+        gene = findSimpleGene(DNA, "ATG", "TAA");
+        System.out.println("DNA strand: " + DNA + "\nGene: " + gene);
+        
+        //has only lower case letters
+        DNA = "atgacgatataatataacccgata";
+        gene = findSimpleGene(DNA, "atg", "taa");
+        System.out.println("DNA strand: " + DNA + "\nGene: " + gene);
+  
         //System.out.println("Finished testSimpleGene");
     }
 }
